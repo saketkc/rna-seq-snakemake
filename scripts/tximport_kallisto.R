51c0f3ba2994f4efc48e24444df8ea88c5377907
Saket Choudhary <saketkc@gmail.com>
2017-11-08 19:00:21 -0800

    Add kallisto importer

---
+++ scripts/tximport_kallisto.R
@@ -0,0 +1,14 @@
+library(EnsDb.Hsapiens.v86)
+library(tximport)
+
+dir <- ''
+
+txdf <- transcripts(EnsDb.Hsapiens.v86, return.type="DataFrame")
+tx2gene <- as.data.frame(txdf[,c("tx_id","gene_id")])
+
+files <- files.path(dir, list.dirs(dir, recursive = F, full.names = F), 'counts',  'abundance.h5')
+names(files) <- list.dirs(dir, recursive = F, full.names = F)
+txi.kallisto.tsv <- tximport(files, type = "kallisto", tx2gene = tx2gene)
+write.table(txi.kallisto.tsv, 'merged_df.tsv', row.names=F)
+
+
