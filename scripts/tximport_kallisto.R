de1a6c564ab2d04ea0b8abdfc79b6fc7d44cf7b4
Saket Choudhary <saketkc@gmail.com>
2017-11-08 19:02:12 -0800

    Add kallisto importer

--- scripts/tximport_kallisto.R
+++ scripts/tximport_kallisto.R
@@ -8,7 +8,7 @@ tx2gene <- as.data.frame(txdf[,c("tx_id","gene_id")])
 
 files <- files.path(dir, list.dirs(dir, recursive = F, full.names = F), 'counts',  'abundance.h5')
 names(files) <- list.dirs(dir, recursive = F, full.names = F)
-txi.kallisto.tsv <- tximport(files, type = "kallisto", tx2gene = tx2gene)
+txi.kallisto.tsv <- tximport(files, type = "kallisto", tx2gene = tx2gene,  ignoreTxVersion = T)
 write.table(txi.kallisto.tsv, 'merged_df.tsv', row.names=F)
 
 
