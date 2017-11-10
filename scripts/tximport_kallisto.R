ef3b05f0ad563fe0b116fbecdc0258dd94bab281
Saket Choudhary <saketkc@gmail.com>
2017-11-10 12:01:01 -0800

    update template

--- scripts/tximport_kallisto.R
+++ scripts/tximport_kallisto.R
@@ -1,5 +1,7 @@
 library(EnsDb.Hsapiens.v86)
 library(tximport)
+library(DESeq2)
+
 
 dir <- ''
 
@@ -9,6 +11,15 @@ tx2gene <- as.data.frame(txdf[,c("tx_id","gene_id")])
 files <- files.path(dir, list.dirs(dir, recursive = F, full.names = F), 'counts',  'abundance.h5')
 names(files) <- list.dirs(dir, recursive = F, full.names = F)
 txi.kallisto.tsv <- tximport(files, type = "kallisto", tx2gene = tx2gene,  ignoreTxVersion = T)
-write.table(txi.kallisto.tsv, 'merged_df.tsv', row.names=F)
+
+sampleTable <- data.frame(condition = factor(rep(c("A", "B"), each = 3)))
+rownames(sampleTable) <- colnames(txi$counts)
+
+dds <- DESeqDataSetFromTximport(txi, sampleTable, ~condition)
+dds <- dds[ rowSums(counts(dds)) > 1, ]
+
+rld <- rlog(dds, blind=FALSE)
+
+write.table(rld, 'merged_df.tsv', row.names=F)
 
 
