314828fd93f00aba96a7763c9433ed0c3c2ba671
Saket Choudhary <saketkc@gmail.com>
2018-03-16 15:11:27 -0700

    Add sh

---
+++ submitall.alt_splicing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source activate clipseq
+echo $PWD
+
+snakemake --js $PWD/jobscript.sh\
+    --snakefile Snakefile_fastq_multilane.alt_splicing\
+    --printshellcmds\
+    --restart-times 2\
+    --cluster-config $PWD/cluster.yaml\
+    --jobname 'make.{jobid}.{rulename}'\
+    --keep-going\
+    --stats $PWD/snakemake.stats\
+    --timestamp\
+    --rerun-incomplete\
+    -j 100\
+    --cluster 'qsub -q cmb -l walltime={cluster.time} -l mem={cluster.mem} -l vmem={cluster.mem} -l pmem={cluster.mem} -l nodes=1:ppn={cluster.cores} -o {cluster.logdir} -e {cluster.logdir}'
---
+++ submitall.fastq_multilane.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source activate clipseq
+echo $PWD
+
+snakemake --snakefile Snakefile_fastq_multilane.snake\
+    --js $PWD/jobscript.sh\
+    --printshellcmds\
+    --restart-times 4\
+    --cluster-config $PWD/cluster.yaml\
+    --jobname 'make.{jobid}.{rulename}'\
+    --keep-going\
+    --stats $PWD/snakemake.stats\
+    --timestamp\
+    --rerun-incomplete\
+    -j 100\
+    --cluster 'qsub -q cmb -l walltime={cluster.time} -l mem={cluster.mem} -l vmem={cluster.mem} -l pmem={cluster.mem} -l nodes=1:ppn={cluster.cores} -o {cluster.logdir} -e {cluster.logdir}'
